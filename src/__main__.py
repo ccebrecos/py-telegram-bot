3cfe9f23bd0d087646ebeabec61b81c3ea069e2a
Carlos GC <mail@ccebrecos.com>
2018-02-07 13:07:23 +0100

    Minor changes

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,10 @@ import logging
 
 # Relative imports
 import src.log
-from src.cli.arguments.constants import LOGS_LEVELS, LOGS
 from src.bot.main import bot
+from src.cli.arguments.constants import LOGS_LEVELS, LOGS
+from src.ws.ws import open_con, close_con
+
 
 # Constants
 LOGGER = logging.getLogger(__name__)
@@ -17,6 +19,8 @@ if __name__ == "__main__":
     root_logger.setLevel(LOGS_LEVELS[LOGS.index('debug')])
     # Welcome
     LOGGER.info("Welcome!")
+    open_con()
     bot.polling()
     # Exiting
+    close_con()
     LOGGER.info("Bye!")
